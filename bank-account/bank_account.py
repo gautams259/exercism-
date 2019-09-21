e322e7c52a1030d9e61a54c5fdb894df3f73a017
deepak <deepak1998it@gmail.com>
2019-09-21 12:45:59 +0530

    repos

---
+++ bank-account/bank_account.py
@@ -0,0 +1,47 @@
+class BankAccount(object):
+    def __init__(self):
+        self.banalce = 0
+        self.ac = False
+
+    def get_balance(self):
+        if self.ac:
+            return self.banalce
+        else:
+            raise Exception("Account closed")
+
+    def open(self):
+        if not self.ac:
+            self.ac = True
+        else:
+            raise Exception("Account already open...")
+
+    def deposit(self, amount):
+        if self.ac and  amount >0:
+            self.banalce = self.banalce + amount
+            return self.banalce
+        else:
+            raise Exception("Can't deposit") 
+
+    def withdraw(self, amount):
+        if self.ac:
+            if amount > 0 and self.banalce >= amount:
+                self.banalce = self.banalce - amount
+                return self.banalce
+            else:
+                raise Exception("Cannot withdraw  ")
+        else:
+            raise Exception("Account closed...") 
+
+    def close(self):
+        if self.ac:
+            self.ac = False
+            self.banalce = 0
+            return self.ac
+        else:
+            raise Exception("Account already closed..")
+
+if __name__ == '__main__':
+    ob = BankAccount()
+    ob.open()
+    ob.get_balance()
+    ob.deposit(3434)
